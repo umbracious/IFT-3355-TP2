c609fe2efb0729495cc528d277adb43c56e23b6a
umbracious <63139410+umbracious@users.noreply.github.com>
2024-03-07 18:10:03 -0500

    idk

--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -9,8 +9,14 @@ void Raytracer::render(const Scene& scene, Frame* output)
     }
 
 	// @@@@@@ VOTRE CODE ICI
+	double FOVy_rads = deg2rad(scene.camera.fovy);
+	double plane_height = tan(FOVy_rads/2)*scene.camera.z_near*2;
+	double plane_width = plane_height * scene.camera.aspect; // Assuming aspect ratio is width:height
+	double3 top_left_corner = scene.camera.center + scene.camera.up*(plane_height/2) 
+		+ cross(scene.camera.center, scene.camera.up)*(plane_width/2); // Left vector is cross product between up and center vectors
+
 	// Calculez les paramètres de la caméra pour les rayons.
-	
+
 
     // Itère sur tous les pixels de l'image.
     for(int y = 0; y < scene.resolution[1]; y++) {
@@ -33,8 +39,16 @@ void Raytracer::render(const Scene& scene, Frame* output)
 
 				// @@@@@@ VOTRE CODE ICI
 				// Mettez en place le rayon primaire en utilisant les paramètres de la caméra.
-				// Lancez le rayon de manière uniformément aléatoire à l'intérieur du pixel dans la zone délimité par jitter_radius. 
-				//Faites la moyenne des différentes couleurs obtenues suite à la récursion.
+				ray.origin = scene.camera.position;
+
+				// Lancez le rayon de manière uniformément aléatoire à l'intérieur du pixel dans la zone délimité par jitter_radius.
+				double3 pixel_pos{-scene.resolution[0]/2 + x + 0.5, scene.resolution[1]/2 - y - 0.5, 1}; // Pixel center position in camera space
+				pixel_pos += double3{-scene.jitter_radius + rand_double()*scene.jitter_radius*2, -scene.jitter_radius + rand_double()*scene.jitter_radius*2, 0}; // Apply jitter to pixel
+				// TODO: Scale to position in plane
+				// TODO: Transform pixel coordinate from camera space to world space
+				ray.direction = normalize(pixel_pos-ray.origin); // Set ray direction (normalized)
+
+				// Faites la moyenne des différentes couleurs obtenues suite à la récursion.
 			}
 
 			avg_z_depth = avg_z_depth / scene.samples_per_pixel;
